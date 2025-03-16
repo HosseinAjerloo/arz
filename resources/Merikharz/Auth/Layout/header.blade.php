d9bfa1d2c4a470c0491328f826b81793f6a06b16
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-03-16 12:06:51 +0330

    sync

--- resources/Merikharz/Auth/Layout/header.blade.php
+++ resources/Merikharz/Auth/Layout/header.blade.php
@@ -2,13 +2,13 @@
     <nav class="px-4 h-full bg-base-bg-color  flex items-center justify-between border-b-base-font-color border-b relative">
 
 
-        <div class="flex space-x-reverse space-x-2">
+        <a href="{{route('panel.index')}}" class="flex space-x-reverse space-x-2">
             <h1 class="font-bold text-xl">
                 مریخ
                 <span class="text-base-font-color">ارز</span>
             </h1>
             <img src="{{asset("merikhArz/src/images/merikhIcon.svg")}}" alt="">
-        </div>
+        </a>
         <div class="flex space-x-reverse space-x-2">
             <h1 class="font-bold text-xl">
                 Merikh
