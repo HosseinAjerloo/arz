9378ee7eca02582c7a10e04e16b123bcc76cde8e
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-03-17 11:14:29 +0330

    sync

--- routes/web.php
+++ routes/web.php
@@ -118,3 +118,27 @@ Route::fallback(function () {
     abort(404);
 });
 
+Route::get('test',function (){
+    $bank=\App\Models\Bank::find(2);
+    $objBank = new $bank->class;
+    $objBank->setTotalPrice(10000);
+    $objBank->setOrderID(7000);
+    $objBank->setBankUrl($bank->url);
+    $objBank->setTerminalId($bank->terminal_id);
+    $objBank->setUrlBack(route('panel.Purchase-through-the-bank'));
+    $objBank->setBankModel($bank);
+    $status = $objBank->payment();
+    return $objBank->connectionToBank($status);
+
+});
+
+Route::get('back-test',function (){
+    $bank=\App\Models\Bank::find(2);
+    $objBank = new $bank->class;
+    dump($objBank->backBank());
+    $objBank->setBankModel($bank);
+    $inputs = array_merge(request()->all(),request()->request->all());
+    $back_price = $objBank->verify(10000);
+    dd($back_price);
+});
+
